9a349259d69532573da29046c640bba0957cb1f0
Olivier02500 <olivier.peuchet@hotmail.com>
2021-11-24 16:35:51 +0100

    make exo 78

--- exercice1.js
+++ exercice1.js
@@ -1,8 +1,23 @@
+
 let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
+let position = document.getElementById('position');
+position.innerText = monTexte.indexOf('i');
+
+let newTexte = monTexte.toUpperCase();
+
+let chaineMaj = document.getElementById('chaineMaj');
+chaineMaj.innerText = newTexte;
+
+let a = newTexte.indexOf('A');
+
+for (let i = 0; i < a; i++) {
+    alert("lettre A trouvré")
+}
+
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
